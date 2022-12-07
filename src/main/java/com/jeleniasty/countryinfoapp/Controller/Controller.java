f714513838dd947f7a66ef4b98b3c80074859dc9
micha <michal.jeleniasty@gmail.com>
2022-12-07 14:39:28 +0100

    initial endpoint check

--- src/main/java/com/jeleniasty/countryinfoapp/Controller/Controller.java
+++ src/main/java/com/jeleniasty/countryinfoapp/Controller/Controller.java
@@ -1,4 +1,15 @@
 package com.jeleniasty.countryinfoapp.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class Controller {
+
+    @GetMapping("countries")
+    public String getCountries() {
+        return "Countries endpoint in development phase";
+    }
 }
