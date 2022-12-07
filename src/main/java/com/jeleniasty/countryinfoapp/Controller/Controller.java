5c84cda3791c34d0443abfff630250e686c74dfd
micha <michal.jeleniasty@gmail.com>
2022-12-07 17:22:51 +0100

    Get and return list of random addresses

--- src/main/java/com/jeleniasty/countryinfoapp/Controller/Controller.java
+++ src/main/java/com/jeleniasty/countryinfoapp/Controller/Controller.java
@@ -1,8 +1,14 @@
 package com.jeleniasty.countryinfoapp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jeleniasty.countryinfoapp.DAO.Address;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -10,6 +16,26 @@ public class Controller {
 
     @GetMapping("countries")
     public String getCountries() {
-        return "Countries endpoint in development phase";
+        return "Initial endpoint check. " +
+                "Countries endpoint in development phase";
+    }
+
+    @RequestMapping ("addresses")
+    public List<Address> getRandomAddress(@RequestParam(value = "number", defaultValue = "5") Integer number) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Address> addresses = new ArrayList<>();
+
+        for (int i = 0; i < number; i++) {
+
+            try {
+                Address address = objectMapper.readValue(new URL("https://random-data-api.com/api/v2/addresses"), Address.class);
+                addresses.add(address);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        return addresses;
     }
 }
