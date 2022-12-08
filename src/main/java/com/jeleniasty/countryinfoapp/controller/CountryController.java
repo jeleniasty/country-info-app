0fa2bbe7e336220248453d9f9a0ce6d2141f9872
micha <michal.jeleniasty@gmail.com>
2022-12-09 00:28:58 +0100

    Input number validation

--- src/main/java/com/jeleniasty/countryinfoapp/controller/CountryController.java
+++ src/main/java/com/jeleniasty/countryinfoapp/controller/CountryController.java
@@ -4,11 +4,14 @@ package com.jeleniasty.countryinfoapp.controller;
 
 import com.jeleniasty.countryinfoapp.dto.CountryResponseDTO;
 import com.jeleniasty.countryinfoapp.service.AddressService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class CountryController {
@@ -16,7 +19,7 @@ public class CountryController {
     private final AddressService addressService;
 
     @GetMapping("/countries")
-    public List<CountryResponseDTO> getCountriesInfo(@RequestParam(value = "number") Integer number) {
+    public List<CountryResponseDTO> getCountriesInfo(@RequestParam(value = "number") @Min(5) @Max(20)  Integer number) {
         return addressService.getCountriesInfo(number);
     }
 
--- src/main/java/com/jeleniasty/countryinfoapp/controller/GlobalExceptionHandler.java
+++ src/main/java/com/jeleniasty/countryinfoapp/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jeleniasty.countryinfoapp.controller;
 
 import com.jeleniasty.countryinfoapp.exception.ExceptionResponse;
 import com.jeleniasty.countryinfoapp.exception.NoInformationFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,5 +14,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoInformationFoundException.class)
     public ResponseEntity<ExceptionResponse> handleException(NoInformationFoundException exception) {
         return new ResponseEntity<>(new ExceptionResponse("No information found!"),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> constraintViolationExceptionHandler(ConstraintViolationException exception) {
+        return new ResponseEntity<>(new ExceptionResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
